9f5b4c97ad844426362a039f11ee41734b6e2266
NickolasBenakis <nickolasbenele@gmail.com>
2018-12-25 12:19:06 +0200

    Modify workspace , Main class Added

--- src/Main.java
+++ src/Main.java
@@ -1,86 +1,16 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Main {
 
-    public static void main(String[] args) {
-
-        int userChoise;
-        String city;
-        Scanner input = new Scanner(System.in);
-
-        userChoise = menu();
-
-        if (userChoise == 1) {
-            System.out.print("City :");
-            city = input.next();
-            getWeatherForCity(city);
+    public static void main(String[] args)  {
 
 
-        } else if (userChoise == 2) {
-            System.out.println("\nMade by Nickolas Benakis\nBorn in 1993");
-            System.out.println("******************************");
-            System.out.println("\n");
-            do {
-                menu();
-            }while (menu() ==2);
+        Ergasia e1 = new Ergasia();
 
-
-
-        } else if (userChoise == 3) {
-            System.out.println("Bye Bye ");
-
-        } else {
-            System.out.println("Error");
-        }
+        e1.menu();
 
 
     }
 
-    public static int menu() {
-        int selection;
-
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Welcome to my Application");
-        System.out.println("-------------------------");
-        System.out.println("1 - Show Weather ");
-        System.out.println("2 - About me");
-        System.out.println("3 - Exit");
-        System.out.print("\nPress : ");
-
-        selection = input.nextInt();
-
-        return selection;
-    }
-
-    public static String getWeatherForCity(String city) {
-        BufferedReader rd = null;
-        StringBuilder sb = null;
-        String line = null;
-        try {
-            URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q="
-                    + city
-                    + "&appid="
-                    + "2a3c7eb60acb02a208a32f66e087284"
-            );
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.connect();
-            rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            sb = new StringBuilder();
-            while ((line = rd.readLine()) != null) {
-                sb.append(line + '\n');
-            }
-            return sb.toString();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "";
-    }
 
 }
